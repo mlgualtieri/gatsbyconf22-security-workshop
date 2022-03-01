7266470ff77c01d1dd52b394f8745b16b1589906
Rodney Matambo <rmatambo8@icloud.com>
2022-03-01 12:28:43 -0400

    Revert "[do not merge yet] Front end fixes"

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,15 +1,17 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 import { isLoggedIn } from "../services/auth"
 import { Link, navigate } from "gatsby"
 
-const ReactDOM = require("react-dom")
+const ReactDOM = require('react-dom')
 
 //import Cookies from 'universal-cookie';
+
 
 export const getCachedData = () =>
   window.localStorage.getItem("dataCache")
     ? JSON.parse(window.localStorage.getItem("dataCache"))
     : {}
+
 
 /*
 export const getFile = (file) => {
@@ -29,105 +31,107 @@ export const getFile = (file) => {
 */
 
 export const getFile = (file, e) => {
-  e.preventDefault()
-  window.open(`/api/docs?file=${file}`, "_blank")
+	e.preventDefault()
+    window.open(`/api/docs?file=${file}`, "_blank")
 }
 
-export const setProfileData = data => {
-  // Populate user data
-  document.getElementById("fullname").innerText = data.fullname
-  document.getElementById("username").innerText = data.username
-  /*
+
+export const setProfileData = (data) => {
+    // Populate user data
+    document.getElementById("fullname").innerText = data.fullname
+    document.getElementById("username").innerText = data.username
+    /*
     	TODO: Replace with react logic
 	- track fullname and username using useState/ use Reducer
 	- pass in data into a list component
 	- separate logic and rendering - conditionally render if list is available
 	
     */
-  // Populate list of docs
-  const doc_items = data.docs.map((file, e) => (
-    <li key={file}>
-      <Link to="#" onClick={e => getFile(file, e)}>
-        <i className="fa fa-file"></i> {file}
-      </Link>
-    </li>
-  ))
-  ReactDOM.render(<ul>{doc_items}</ul>, document.getElementById("docs"))
+    // Populate list of docs
+    const doc_items = data.docs.map((file, e) =>  
+        <li key={file}>
+            <Link to="#" onClick={(e) => getFile(file, e)}>
+                <i className="fa fa-file"></i> {file}
+            </Link>
+        </li>)
+    ReactDOM.render(
+        <ul>{doc_items}</ul>, document.getElementById('docs')
+    );
 }
+
 
 export const getData = data => {
-  let postData = {}
 
-  //const cookies = new Cookies();
-  //postData.csrf_token = cookies.get('csrf_token')
+    let postData = {}
+    
+    //const cookies = new Cookies();
+    //postData.csrf_token = cookies.get('csrf_token')
+    
+    if (isLoggedIn()) {
+ 
+        // If cached data is present retrieve
+        let cachedData = getCachedData()
+        if(Object.keys(cachedData).length !== 0) {
+            setProfileData(cachedData)
+        }
+        else {
+            // Show spinner while we wait for the data // TODO: create component for spinner and render spinning when getting data
+            document.getElementById("fullname").innerHTML = `<i class="fas fa-spinner fa-spin"></i>`
+            document.getElementById("username").innerHTML = `<i class="fas fa-spinner fa-spin"></i>` 
+            document.getElementById("docs").innerHTML     = `<i class="fas fa-spinner fa-spin"></i>` 
+        }
 
-  if (isLoggedIn()) {
-    // If cached data is present retrieve
-    let cachedData = getCachedData()
-    if (Object.keys(cachedData).length !== 0) {
-      setProfileData(cachedData)
-    } else {
-      // Show spinner while we wait for the data // TODO: create component for spinner and render spinning when getting data
-      document.getElementById(
-        "fullname"
-      ).innerHTML = `<i class="fas fa-spinner fa-spin"></i>`
-      document.getElementById(
-        "username"
-      ).innerHTML = `<i class="fas fa-spinner fa-spin"></i>`
-      document.getElementById(
-        "docs"
-      ).innerHTML = `<i class="fas fa-spinner fa-spin"></i>`
+        // Update with the dashboard server-side data
+        fetch(`/api/dashboard`, {
+            method: 'POST',
+            headers: {
+              "content-type": `application/json`,
+            },
+            body: JSON.stringify(postData)
+        })
+        .then(response => {
+            if(response.status !== 200) {
+                navigate("/app/logout")
+                return null
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(resultData => {
+            if(resultData !== null) {
+                //console.log(resultData)
+                setProfileData(resultData)
+                window.localStorage.setItem("dataCache", JSON.stringify(resultData)) // local storage calls are expensive
+            }
+        }) 
+    }
+    else {
+        console.log("Not logged in...")
     }
 
-    // Update with the dashboard server-side data
-    fetch(`/api/dashboard`, {
-      method: "POST",
-      headers: {
-        "content-type": `application/json`,
-      },
-      body: JSON.stringify(postData),
-    })
-      .then(response => {
-        if (response.status !== 200) {
-          navigate("/app/logout")
-          return null
-        } else {
-          return response.json()
-        }
-      })
-      .then(resultData => {
-        if (resultData !== null) {
-          //console.log(resultData)
-          setProfileData(resultData)
-          window.localStorage.setItem("dataCache", JSON.stringify(resultData)) // local storage calls are expensive
-        }
-      })
-  } else {
-    console.log("Not logged in...")
-  }
 }
 
+
 const Dashboard = () => {
-  React.useEffect(() => {
-    getData()
-  }, [])
+
+    useEffect(() => {
+          getData();
+    }, []);
 
   return (
-    <>
-      <h2>Account</h2>
-      <ul>
-        <li>
-          <b>Name:</b> <span id="fullname"></span>
-        </li>
-        <li>
-          <b>Email:</b> <span id="username"></span>
-        </li>
-      </ul>
-      <h2>Documents</h2>
-      <div id="docs"></div>
-    </>
+  <>
+    <h2>Account</h2>
+    <ul>
+      <li><b>Name:</b> <span id="fullname"></span></li>
+      <li><b>Email:</b> <span id="username"></span></li>
+    </ul>
+    <h2>Documents</h2>
+    <div id="docs">
+    </div>
+  </>
   )
 }
 
-//<li>E-mail: {getUser().email}</li>
+      //<li>E-mail: {getUser().email}</li>
 export default Dashboard
