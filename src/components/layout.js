71b5afd4de3d123df3430ee593d484f861ca8d9c
Mike Gualtieri <mike.gualtieri@gatsbyjs.com>
2022-03-01 22:15:35 -0500

    CSP/SRI demo script

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,6 +24,8 @@ const Layout = ({ pageTitle, children }) => {
     }
   `)
 
+  // Script to demo SRI/CSP
+  //<script type="text/javascript" src="https://www.mike-gualtieri.com/js/demo.js" integrity="sha384-jqpsumwdyCmsgWf0wSeEsw8wjr869wcth6V4hIazq3L94fm/ea7zCjnhvwtA/CNe" crossorigin="anonymous"></script>
   let output = (
     <>
     <div className={container}>
@@ -32,7 +34,7 @@ const Layout = ({ pageTitle, children }) => {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mulish:300,400" />
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self' 127.0.0.1:* ws://127.0.0.1:*; script-src 'self' 'unsafe-eval'; style-src 'self' *.bootstrapcdn.com fonts.googleapis.com cdnjs.cloudflare.com 'unsafe-inline'; font-src cdnjs.cloudflare.com fonts.gstatic.com; img-src 'self' data:; child-src 'none';" />
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self' 127.0.0.1:* ws://127.0.0.1:*; script-src 'self' 'unsafe-eval' www.mike-gualtieri.com; style-src 'self' *.bootstrapcdn.com fonts.googleapis.com cdnjs.cloudflare.com 'unsafe-inline'; font-src cdnjs.cloudflare.com fonts.gstatic.com; img-src 'self' data:; child-src 'none';" />
       </Helmet>
       <NavBar />
       <main>
